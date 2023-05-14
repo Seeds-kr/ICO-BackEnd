279abe865dfcc48d9c4158b840f467d742d48abb
이규훈 <wxyz0904@naver.com>
2023-05-14 20:44:59 +0900

    기본 세팅

---
+++ .idea/ilovecoding/src/main/java/com/example/ilovecoding/IlovecodingApplication.java
@@ -0,0 +1,13 @@
+package com.example.ilovecoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlovecodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IlovecodingApplication.class, args);
+	}
+
+}
